0625f12da5e22505532e8058cbb3849f678fffd0
Matthew Caruana Galizia <mattcg@gmail.com>
2013-04-23 14:58:34 -0600

    Switch based on node version

--- example-tor.js
+++ example-tor.js
@@ -13,25 +13,30 @@ var options = {
 };
 
 var req = http.request(options, function(res) {
+	var version;
+
 	console.log('----------- STATUS -----------');
 	console.log(res.statusCode);
 	console.log('----------- HEADERS ----------');
 	console.log(JSON.stringify(res.headers));
 	res.setEncoding('utf8');
 
-	// The new way, using the readable stream interface (Node >= 0.10.0):
-	res.on('readable', function() {
-		console.log('----------- CHUNK ------------');
-		console.log(res.read());
-	});
+	version = process.version.substr(1).split('.');
+	if (version[0] > 0 || version[1] > 8) {
 
-	/*
-	// The old way, using 'data' listeners (Node <= 0.8.22):
-	res.on('data', function(chunk) {
-		console.log('----------- CHUNK ------------');
-		console.log(chunk);
-	});
-	*/
+		// The new way, using the readable stream interface (Node >= 0.10.0):
+		res.on('readable', function() {
+			console.log('----------- CHUNK ------------');
+			console.log(res.read());
+		});
+	} else {
+
+		// The old way, using 'data' listeners (Node <= 0.8.22):
+		res.on('data', function(chunk) {
+			console.log('----------- CHUNK ------------');
+			console.log(chunk);
+		});
+	}
 });
 
 req.on('error', function(e) {
